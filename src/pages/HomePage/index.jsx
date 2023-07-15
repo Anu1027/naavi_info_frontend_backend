0e6deb7e2daf3443f8d1cf814f64a80ab4c497e4
Saravanan M <sarvan.m20@gmail.com>
2023-07-15 19:20:01 +0530

    updated home page

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./homepage.scss";
+
 import logo from "../../static/images/logo.svg";
+import homepageImg from "../../static/images/homepageImg.svg";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -28,20 +30,33 @@ const HomePage = () => {
           <div>
             <p>Vendors</p>
           </div>
-          <div className="btns-div">
-            <div
-              className="business-btn"
-              onClick={() => {
-                navigate("/dashboard");
-              }}
-            >
-              Business
-            </div>
-            <div className="gs-Btn">Get Started</div>
-          </div>
+        </div>
+        <div className="btns-div">
+          {/* <div
+            className="business-btn"
+            onClick={() => {
+              navigate("/dashboard");
+            }}
+          >
+            Business
+          </div> */}
+          <div className="gs-Btn">Get Started</div>
         </div>
       </div>
       <div className="color-box"></div>
+      <div className="cover-Img">
+        <img src={homepageImg} alt="" />
+        <div className="background-tint"></div>
+        <div className="mid-text">Find Your Next Adventure</div>
+        <div className="background-tint1"></div>
+        <div className="input-box1">
+          <input type="text" placeholder="What Do You Want To Accomplish?" />
+        </div>
+        <div className="input-box2">
+          <input type="text" placeholder="By When?" />
+        </div>
+        <div className="createPath-btn">Create Path</div>
+      </div>
     </div>
   );
 };
