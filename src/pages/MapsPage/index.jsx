fd9d1ebe8a94eea9624978a55898cafe646a93a9
Saravanan M <sarvan.m20@gmail.com>
2023-07-18 12:03:13 +0530

    updated maps page

--- src/pages/MapsPage/index.jsx
+++ src/pages/MapsPage/index.jsx
@@ -8,10 +8,60 @@ import careerIcon from "../../static/images/mapspage/careerIcon.svg";
 import educationIcon from "../../static/images/mapspage/educationIcon.svg";
 import immigrationIcon from "../../static/images/mapspage/immigrationIcon.svg";
 import plus from "../../static/images/mapspage/plus.svg";
+import close from "../../static/images/mapspage/close.svg";
 
 const MapsPage = () => {
   const navigate = useNavigate();
   const [option, setOption] = useState("Career");
+  const [containers, setContainers] = useState([
+    { id: 1, inputValue1: "", inputValue2: "", removable: false },
+  ]);
+
+  const handleAddContainer = () => {
+    const lastContainer = containers[containers.length - 1];
+    const newContainerId = lastContainer.id + 1;
+    const newContainer = {
+      id: newContainerId,
+      inputValue1: "",
+      inputValue2: "",
+      removable: true,
+    };
+    setContainers([...containers, newContainer]);
+  };
+
+  const handleRemoveContainer = (containerId) => {
+    const updatedContainers = containers.filter(
+      (container) => container.id !== containerId
+    );
+    // Renumber the containers after removing one
+    const renumberedContainers = updatedContainers.map((container, index) => {
+      return { ...container, id: index + 1 };
+    });
+    setContainers(renumberedContainers);
+  };
+
+  const handleInputChange = (e, containerId, inputIndex) => {
+    const updatedContainers = [...containers];
+    const containerIndex = updatedContainers.findIndex(
+      (container) => container.id === containerId
+    );
+
+    if (containerIndex !== -1) {
+      if (inputIndex === 1) {
+        updatedContainers[containerIndex].inputValue1 = e.target.value;
+      } else if (inputIndex === 2) {
+        updatedContainers[containerIndex].inputValue2 = e.target.value;
+      }
+
+      setContainers(updatedContainers);
+    }
+  };
+
+  const handleResetContainer = () => {
+    setContainers([
+      { id: 1, inputValue1: "", inputValue2: "", removable: false },
+    ]);
+  };
 
   return (
     <div className="mapspage">
@@ -76,7 +126,7 @@ const MapsPage = () => {
               <div
                 className="icon-name-txt"
                 style={{
-                  fontWeight: option === "Career" ? "500" : "",
+                  fontWeight: option === "Career" ? "600" : "",
                 }}
               >
                 Career
@@ -102,7 +152,7 @@ const MapsPage = () => {
               <div
                 className="icon-name-txt"
                 style={{
-                  fontWeight: option === "Education" ? "500" : "",
+                  fontWeight: option === "Education" ? "600" : "",
                 }}
               >
                 Education
@@ -128,7 +178,7 @@ const MapsPage = () => {
               <div
                 className="icon-name-txt"
                 style={{
-                  fontWeight: option === "Immigration" ? "500" : "",
+                  fontWeight: option === "Immigration" ? "600" : "",
                 }}
               >
                 Immigration
@@ -139,7 +189,35 @@ const MapsPage = () => {
             <div className="input-div1">
               <input type="text" placeholder="Choose Starting Coordinates.." />
             </div>
-            <div className="destination-container">
+            {containers.map((container, index) => (
+              <div className="destination-container" key={container.id}>
+                <div className="dest-txt">
+                  <div>Destination {container.id}</div>
+                  {container.removable && (
+                    <div onClick={() => handleRemoveContainer(container.id)}>
+                      <img src={close} alt="" />
+                    </div>
+                  )}
+                </div>
+                <div className="input-div2">
+                  <input
+                    type="text"
+                    value={container.inputValue1}
+                    placeholder="Where Do You Want To Go?"
+                    onChange={(e) => handleInputChange(e, container.id, 1)}
+                  />
+                </div>
+                <div className="input-div2">
+                  <input
+                    type="text"
+                    value={container.inputValue2}
+                    placeholder="By When?"
+                    onChange={(e) => handleInputChange(e, container.id, 2)}
+                  />
+                </div>
+              </div>
+            ))}
+            {/* <div className="destination-container">
               <div className="dest-txt">Destination 1</div>
               <div className="input-div2">
                 <input type="text" placeholder="Where Do You Want To Go?" />
@@ -147,14 +225,14 @@ const MapsPage = () => {
               <div className="input-div2">
                 <input type="text" placeholder="By When?" />
               </div>
-            </div>
-            <div className="add-div">
+            </div> */}
+            <div className="add-div" onClick={handleAddContainer}>
               <img src={plus} alt="" />
               Add Destination
             </div>
             <div className="maps-btns-div">
               <div className="gs-Btn-maps">Get Started</div>
-              <div className="reset-btn">Reset</div>
+              <div className="reset-btn" onClick={handleResetContainer}>Reset</div>
             </div>
           </div>
         </div>
