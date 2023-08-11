ad9603e879f47b062287be0d546264ac9458c29c
Saravanan M <sarvan.m20@gmail.com>
2023-08-11 18:57:19 +0530

    path component updated with google maps

--- src/components/PathComponent/MapComponent.jsx
+++ src/components/PathComponent/MapComponent.jsx
@@ -1,106 +1,27 @@
 import React, { useEffect, useState } from "react";
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Annotation,
-  ZoomableGroup,
-  Sphere,
-  Graticule,
-} from "react-simple-maps";
-import { scaleLinear } from "d3-scale";
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
 import "./mapcomponent.scss";
 
-const geoUrl =
-  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
-
-const MapComponent = () => {
-  const [center, setCenter] = useState([0, 0]);
-  const [zoom, setZoom] = useState(1);
-  const [hoveredCountry, setHoveredCountry] = useState(null);
-
-  const handleGeographyClick = (geo) => {
-    const newZoom = zoom === 1 ? 2 : 1;
-    const newCenter = geo.properties.centroid;
-    setZoom(newZoom);
-    setCenter(newCenter);
-  };
-
-  const handleGeographyHover = (geo) => {
-    if (geo?.properties && geo?.properties?.name && geo?.properties?.centroid) {
-      setHoveredCountry(geo?.properties?.name);
-    } else {
-      setHoveredCountry(null);
-    }
-  };
-
+const MapComponent = ({
+  pathMap,
+  setPathMap,
+  pathSearchTerm,
+  pathCurrentLocation,
+  setPathCurrentLocation,
+}) => {
   return (
     <div className="map-component1">
-      <ComposableMap>
-        <ZoomableGroup center={center} zoom={zoom}>
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <React.Fragment key={geo.rsmKey}>
-                  <Geography
-                    geography={geo}
-                    onClick={() => handleGeographyClick(geo)}
-                    onMouseEnter={() => {
-                      // console.log("Hovered: ", geo?.properties?.name);
-                      handleGeographyHover(geo);
-                    }}
-                    onMouseLeave={() => {
-                      // console.log("Left: ", geo?.properties?.name);
-                      setHoveredCountry(null);
-                    }}
-                    style={{
-                      hover: {
-                        fill: "#46B2D3",
-                        outline: "none",
-                      },
-                      pressed: {
-                        fill: "#E42",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                  {hoveredCountry === geo?.properties?.name && (
-                    <Annotation
-                      subject={geo?.properties?.centroid}
-                      dx={0}
-                      dy={-20}
-                      connectorProps={{
-                        stroke: "#FF5533",
-                        strokeWidth: 2,
-                        strokeLinecap: "round",
-                      }}
-                      style={{
-                        border: "1px solid red",
-                        width: '100%',
-                        height: '100%',
-                      }}
-                    >
-                      <text
-                        fontSize={12}
-                        textAnchor="middle"
-                        style={{
-                          fill: "#D0D0D0",
-                          pointerEvents: "none",
-                          backgroundColor: "#323232",
-                          padding: "5px",
-                          borderRadius: "5px",
-                        }}
-                      >
-                        {geo?.properties?.name}
-                      </text>
-                    </Annotation>
-                  )}
-                </React.Fragment>
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
+      <div className="map-content1">
+        <GoogleMap
+          center={pathCurrentLocation}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          onLoad={(pathMap) => setPathMap(pathMap)}
+        >
+          {pathCurrentLocation && <Marker position={pathCurrentLocation} />}
+        </GoogleMap>
+      </div>
+      {/* <div className="place-details1"></div> */}
     </div>
   );
 };
--- src/components/PathComponent/index.jsx
+++ src/components/PathComponent/index.jsx
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import MapComponent from "./MapComponent";
+import Listview from "../Listview";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
+import { LoadScript } from "@react-google-maps/api";
 import "./mapspage.scss";
 
 //images
@@ -10,8 +19,8 @@ import immigrationIcon from "../../static/images/mapspage/immigrationIcon.svg";
 import plus from "../../static/images/mapspage/plus.svg";
 import close from "../../static/images/mapspage/close.svg";
 import hamIcon from "../../static/images/icons/hamIcon.svg";
-import MapComponent from "./MapComponent";
-import Listview from "../Listview";
+
+const libraries = ["places"];
 
 const PathComponent = () => {
   const navigate = useNavigate();
@@ -20,7 +29,14 @@ const PathComponent = () => {
     { id: 1, inputValue1: "", inputValue2: "", removable: false },
   ]);
   const [pathOption, setPathOption] = useState("Map View");
-  const [searchTerm, setSearchterm] = useState('');
+  const [searchTerm, setSearchterm] = useState("");
+  const [pathMap, setPathMap] = useState(/** @type google.maps.Map */ (null));
+  const [pathCurrentLocation, setPathCurrentLocation] = useState(null);
+  const [pathSearchTerm, setPathSearchTerm] = useState("");
+  const autocompleteRef = useRef(null);
+  const [pathResetLoaction, setPathResetLocation] = useState(false);
+  const [pathSelectedPlace, setPathSelectedPlace] = useState(null);
+  const [pathPlacesId, setPathPlacesId] = useState(null);
 
   const handleAddContainer = () => {
     const lastContainer = containers[containers.length - 1];
@@ -62,205 +78,291 @@ const PathComponent = () => {
     }
   };
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      setPathSelectedPlace("");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setPathCurrentLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error(
+            "Error getting current location in path component:",
+            error
+          );
+        }
+      );
+    }
+  }, [pathResetLoaction]);
+
   const handleResetContainer = () => {
     setContainers([
       { id: 1, inputValue1: "", inputValue2: "", removable: false },
     ]);
-    if(pathOption === 'List View') {
-      setSearchterm('');
+    if (pathOption === "List View") {
+      setSearchterm("");
+    }
+    setPathResetLocation(!pathResetLoaction);
+  };
+
+  const handlePlaceSelect = () => {
+    if (autocompleteRef?.current) {
+      const place = autocompleteRef?.current?.getPlace();
+      if (place?.geometry && place?.geometry?.location) {
+        const location = {
+          lat: place?.geometry?.location?.lat(),
+          lng: place?.geometry?.location?.lng(),
+        };
+        setPathCurrentLocation(location);
+        setPathSelectedPlace(place?.formatted_address);
+        const placeId = place?.place_id;
+        setPathPlacesId(placeId);
+        if (pathMap) {
+          pathMap.panTo(location);
+        }
+      }
     }
   };
 
+  const fetchPlaceDetails = async (placeId) => {
+    if (placeId !== null) {
+      try {
+        const response = await fetch(
+          `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=AIzaSyB5MJ2jMHzl_ghkbxOsyPmeBmYw_sUsIRQ`
+        );
+        const data = await response.json();
+        console.log(data?.result, "place info");
+        return data.result;
+      } catch (error) {
+        console.log(error, "error in getting place info");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchPlaceDetails(pathPlacesId);
+  }, [pathPlacesId]);
+
   return (
     <div className="mapspage1">
-      <div className="maps-container1">
-        <div className="maps-sidebar1">
-          <div className="top-icons1">
-            <div
-              className="each-icon1"
-              // onClick={() => {
-              //   setOption("Career");
-              // }}
-              style={{
-                cursor: "not-allowed",
-                opacity: 0.5,
-              }}
-            >
+      <LoadScript
+        googleMapsApiKey="AIzaSyB5MJ2jMHzl_ghkbxOsyPmeBmYw_sUsIRQ"
+        libraries={libraries}
+      >
+        <div className="maps-container1">
+          <div className="maps-sidebar1">
+            <div className="top-icons1">
               <div
-                className="border-div1"
+                className="each-icon1"
+                // onClick={() => {
+                //   setOption("Career");
+                // }}
                 style={{
-                  border:
-                    option === "Career"
-                      ? "1px solid #100F0D"
-                      : "1px solid #e7e7e7",
+                  cursor: "not-allowed",
+                  opacity: 0.5,
                 }}
               >
-                <img src={careerIcon} alt="" />
+                <div
+                  className="border-div1"
+                  style={{
+                    border:
+                      option === "Career"
+                        ? "1px solid #100F0D"
+                        : "1px solid #e7e7e7",
+                  }}
+                >
+                  <img src={careerIcon} alt="" />
+                </div>
+                <div
+                  className="icon-name-txt1"
+                  style={{
+                    fontWeight: option === "Career" ? "600" : "",
+                  }}
+                >
+                  Career
+                </div>
               </div>
               <div
-                className="icon-name-txt1"
-                style={{
-                  fontWeight: option === "Career" ? "600" : "",
+                className="each-icon1"
+                onClick={() => {
+                  setOption("Education");
                 }}
               >
-                Career
+                <div
+                  className="border-div1"
+                  style={{
+                    border:
+                      option === "Education"
+                        ? "1px solid #100F0D"
+                        : "1px solid #e7e7e7",
+                  }}
+                >
+                  <img src={educationIcon} alt="" />
+                </div>
+                <div
+                  className="icon-name-txt1"
+                  style={{
+                    fontWeight: option === "Education" ? "600" : "",
+                  }}
+                >
+                  Education
+                </div>
+              </div>
+              <div
+                className="each-icon1"
+                // onClick={() => {
+                //   setOption("Immigration");
+                // }}
+                style={{
+                  cursor: "not-allowed",
+                  opacity: 0.5,
+                }}
+              >
+                <div
+                  className="border-div1"
+                  style={{
+                    border:
+                      option === "Immigration"
+                        ? "1px solid #100F0D"
+                        : "1px solid #e7e7e7",
+                  }}
+                >
+                  <img src={immigrationIcon} alt="" />
+                </div>
+                <div
+                  className="icon-name-txt1"
+                  style={{
+                    fontWeight: option === "Immigration" ? "600" : "",
+                  }}
+                >
+                  Immigration
+                </div>
               </div>
             </div>
-            <div
-              className="each-icon1"
-              onClick={() => {
-                setOption("Education");
-              }}
-            >
-              <div
-                className="border-div1"
-                style={{
-                  border:
-                    option === "Education"
-                      ? "1px solid #100F0D"
-                      : "1px solid #e7e7e7",
-                }}
-              >
-                <img src={educationIcon} alt="" />
+            <div className="mid-area1">
+              <div className="input-div-1">
+                <input
+                  type="text"
+                  placeholder="Choose Starting Coordinates.."
+                />
               </div>
-              <div
-                className="icon-name-txt1"
-                style={{
-                  fontWeight: option === "Education" ? "600" : "",
-                }}
-              >
-                Education
+              {containers.map((container, index) => (
+                <div className="destination-container1" key={container.id}>
+                  <div className="dest-txt1">
+                    <div>Destination {container.id}</div>
+                    {container.removable && (
+                      <div onClick={() => handleRemoveContainer(container.id)}>
+                        <img src={close} alt="" />
+                      </div>
+                    )}
+                  </div>
+                  <div className="input-div-2">
+                    <Autocomplete
+                      onLoad={(autocomplete) => {
+                        autocompleteRef.current = autocomplete;
+                        autocomplete?.setBounds(pathMap?.getBounds());
+                      }}
+                      onPlaceChanged={handlePlaceSelect}
+                    >
+                      <input
+                        type="text"
+                        placeholder="Where Do You Want To Go?"
+                        // value={container.inputValue1}
+                        // onChange={(e) => {
+                        //   handleInputChange(e, container.id, 1);
+                        //   if (pathOption === "List View") {
+                        //     setSearchterm(e.target.value);
+                        //   }
+                        // }}
+                        value={pathSelectedPlace || ""}
+                        onChange={(e) => {
+                          handleInputChange(e, container.id, 1);
+                          setPathSelectedPlace(e.target.value);
+                        }}
+                      />
+                    </Autocomplete>
+                  </div>
+                  <div className="input-div-2">
+                    <input
+                      type="text"
+                      value={container.inputValue2}
+                      placeholder="By When?"
+                      onChange={(e) => handleInputChange(e, container.id, 2)}
+                    />
+                  </div>
+                </div>
+              ))}
+              <div className="add-div1" onClick={handleAddContainer}>
+                <img src={plus} alt="" />
+                Add Destination
               </div>
-            </div>
-            <div
-              className="each-icon1"
-              // onClick={() => {
-              //   setOption("Immigration");
-              // }}
-              style={{
-                cursor: "not-allowed",
-                opacity: 0.5,
-              }}
-            >
-              <div
-                className="border-div1"
-                style={{
-                  border:
-                    option === "Immigration"
-                      ? "1px solid #100F0D"
-                      : "1px solid #e7e7e7",
-                }}
-              >
-                <img src={immigrationIcon} alt="" />
-              </div>
-              <div
-                className="icon-name-txt1"
-                style={{
-                  fontWeight: option === "Immigration" ? "600" : "",
-                }}
-              >
-                Immigration
+              <div className="maps-btns-div1">
+                <div className="gs-Btn-maps1">Get Started</div>
+                <div className="reset-btn1" onClick={handleResetContainer}>
+                  Reset
+                </div>
               </div>
             </div>
           </div>
-          <div className="mid-area1">
-            <div className="input-div-1">
-              <input type="text" placeholder="Choose Starting Coordinates.." />
-            </div>
-            {containers.map((container, index) => (
-              <div className="destination-container1" key={container.id}>
-                <div className="dest-txt1">
-                  <div>Destination {container.id}</div>
-                  {container.removable && (
-                    <div onClick={() => handleRemoveContainer(container.id)}>
-                      <img src={close} alt="" />
-                    </div>
-                  )}
+          <div className="maps-content-area1">
+            <div className="path-options-div">
+              <div className="path-options">
+                <div
+                  className="each-path-opt"
+                  onClick={() => {
+                    setPathOption("Map View");
+                  }}
+                  style={{
+                    background: pathOption === "Map View" ? "#F1F4F6" : "",
+                  }}
+                >
+                  Map View
                 </div>
-                <div className="input-div-2">
-                  <input
-                    type="text"
-                    value={container.inputValue1}
-                    placeholder="Where Do You Want To Go?"
-                    onChange={(e) => {
-                      handleInputChange(e, container.id, 1);
-                      if(pathOption === 'List View') {
-                        setSearchterm(e.target.value);
-                      }
-                    }}
-                  />
+                <div
+                  className="each-path-opt"
+                  onClick={() => {
+                    setPathOption("Mentor View");
+                  }}
+                  style={{
+                    background: pathOption === "Mentor View" ? "#F1F4F6" : "",
+                  }}
+                >
+                  Mentor View
                 </div>
-                <div className="input-div-2">
-                  <input
-                    type="text"
-                    value={container.inputValue2}
-                    placeholder="By When?"
-                    onChange={(e) => handleInputChange(e, container.id, 2)}
-                  />
+                <div
+                  className="each-path-opt"
+                  onClick={() => {
+                    setPathOption("List View");
+                  }}
+                  style={{
+                    background: pathOption === "List View" ? "#F1F4F6" : "",
+                  }}
+                >
+                  List View
                 </div>
               </div>
-            ))}
-            <div className="add-div1" onClick={handleAddContainer}>
-              <img src={plus} alt="" />
-              Add Destination
             </div>
-            <div className="maps-btns-div1">
-              <div className="gs-Btn-maps1">Get Started</div>
-              <div className="reset-btn1" onClick={handleResetContainer}>
-                Reset
-              </div>
-            </div>
+            <>
+              {pathOption === "Map View" ? (
+                <MapComponent
+                  pathMap={pathMap}
+                  setPathMap={setPathMap}
+                  pathSearchTerm={pathSearchTerm}
+                  pathCurrentLocation={pathCurrentLocation}
+                  setPathCurrentLocation={setPathCurrentLocation}
+                />
+              ) : pathOption === "List View" ? (
+                <Listview searchTerm={searchTerm} />
+              ) : (
+                ""
+              )}
+            </>
           </div>
         </div>
-        <div className="maps-content-area1">
-          <div className="path-options-div">
-            <div className="path-options">
-              <div
-                className="each-path-opt"
-                onClick={() => {
-                  setPathOption("Map View");
-                }}
-                style={{
-                  background: pathOption === "Map View" ? "#F1F4F6" : "",
-                }}
-              >
-                Map View
-              </div>
-              <div
-                className="each-path-opt"
-                onClick={() => {
-                  setPathOption("Mentor View");
-                }}
-                style={{
-                  background: pathOption === "Mentor View" ? "#F1F4F6" : "",
-                }}
-              >
-                Mentor View
-              </div>
-              <div
-                className="each-path-opt"
-                onClick={() => {
-                  setPathOption("List View");
-                }}
-                style={{
-                  background: pathOption === "List View" ? "#F1F4F6" : "",
-                }}
-              >
-                List View
-              </div>
-            </div>
-          </div>
-          <>
-            {pathOption === "Map View" ? (
-              <MapComponent />
-            ) : pathOption === "List View" ? (
-              <Listview searchTerm={searchTerm} />
-            ) : (
-              ""
-            )}
-          </>
-        </div>
-      </div>
+      </LoadScript>
     </div>
   );
 };
