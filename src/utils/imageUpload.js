89361f43a89378a0f3af509b4d4df8730ad23982
Saravanan M <sarvan.m20@gmail.com>
2023-09-02 10:14:28 +0530

    file upload api changed

--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as jose from 'jose';
+import { predefinedToast } from './toast';
 
 const secret = 'uyrw7826^&(896GYUFWE&*#GBjkbuaf'; // secret not to be disclosed anywhere.
 const emailDev = 'pavithran@inr.group'; // email of the developer.
@@ -36,8 +37,17 @@ export const uploadImageFunc = async (e, setImage, setLoading) => {
 
   const jwts = await signJwt(fileName, emailDev, secret);
   // console.log(jwts, 'lkjkswedcf');
+  // let { data } = await axios.post(
+  //   `https://drivetest.globalxchange.io/file/dev-upload-file?email=${emailDev}&path=${path_inside_brain}&token=${jwts}&name=${fileName}`,
+  //   formData,
+  //   {
+  //     headers: {
+  //       'Access-Control-Allow-Origin': '*',
+  //     },
+  //   }
+  // );
   let { data } = await axios.post(
-    `https://drivetest.globalxchange.io/file/dev-upload-file?email=${emailDev}&path=${path_inside_brain}&token=${jwts}&name=${fileName}`,
+    `https://insurance.apimachine.com/insurance/general/upload`,
     formData,
     {
       headers: {
@@ -45,7 +55,25 @@ export const uploadImageFunc = async (e, setImage, setLoading) => {
       },
     }
   );
-  console.log(data.payload.url, 'drive uploaded img');
-  setImage(data.payload.url);
-  setLoading(false);
+  if (data?.length > 0) {
+    console.log(data[0], 'dfile name upload');
+    setImage(
+      // (prev) => {
+      // console.log(
+      //   { ...prev, eachItem: data[0]?.originalname },
+      //   "{ ...prev, eachItem: data[0]?.originalname }"
+      // );
+      // return { ...prev, [eachItem]: data[0]?.originalname };
+      // }
+      data[0]?.urlName
+    );
+    setLoading(false);
+    return data[0]?.urlName;
+  } else {
+    predefinedToast('Error while uploading file');
+    setLoading(false);
+  }
+  // console.log(data.payload.url, 'drive uploaded img');
+  // setImage(data.payload.url);
+  // setLoading(false);
 };
