d4dbddc70e903141e1e311b2b7267645da51424f
Saravanan M <sarvan.m20@gmail.com>
2023-11-01 19:32:52 +0530

    world map svg issue fixed

--- src/pages/MapsPage/MapComponent.jsx
+++ src/pages/MapsPage/MapComponent.jsx
@@ -8,15 +8,22 @@ import {
   Sphere,
   Graticule,
 } from "react-simple-maps";
-// import { scaleLinear } from "d3-scale";
-import "./mapstyle.scss";
+import { scaleLinear } from "d3-scale";
+import { csv } from "d3-fetch";
+import "./mapcomponent.scss";
+import worldCountriesData from "./message.json";
 
 const geoUrl =
   "https://bitbucket.org/globalxchange/topology/raw/82563f00ca0d655d4de61a65688c10e91585d8e7/world-countries.json";
 
+const colorScale = scaleLinear()
+  .domain([0.29, 0.68])
+  .range(["#ffedea", "#ff5233"]);
+
 const MapComponent = () => {
   const [center, setCenter] = useState([0, 0]);
   const [zoom, setZoom] = useState(1);
+  const [data, setData] = useState([]);
 
   const handleGeographyClick = (geo) => {
     const newZoom = zoom === 1 ? 2 : 1;
@@ -26,23 +33,34 @@ const MapComponent = () => {
   };
 
   return (
-    <div className="mapStyle">
-      <ComposableMap>
-        <ZoomableGroup center={center} zoom={zoom}>
-          {/* <Sphere stroke="#E4E5E6" strokeWidth={1} />
-          <Graticule stroke="#E4E5E6" strokeWidth={1} /> */}
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
+    <div className="map-component">
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+      >
+        <Geographies geography={worldCountriesData}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              return (
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
                   onClick={() => handleGeographyClick(geo)}
+                  style={{
+                    default: {
+                      fill: "#59a2dd",
+                    },
+                    hover: {
+                      fill: "#343c5b",
+                    },
+                  }}
                 />
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
+              );
+            })
+          }
+        </Geographies>
       </ComposableMap>
     </div>
   );
